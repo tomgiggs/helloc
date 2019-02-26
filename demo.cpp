fabf0409fb540a4a527961660812ab30968738d5
DESKTOP-Q5QVR0H\giggs <zamjbn@163.com>
2019-02-26 23:50:11 +0800

    init repo

---
+++ demo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main()
+{
+    std::cout<<"hello world";
+    return 0;
+    system("pause");
+}
+
+
+
+// #include <stdio.h>
+// void main(){
+//     printf("hello world");
+// }
+
+
+
